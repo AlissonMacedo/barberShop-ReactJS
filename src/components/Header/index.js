7de61a705e74b4c3cfe480b61eecce78d13e8f30
Alisson Macedo <alissonmacedo@Alissons-MacBook-Pro.local>
2019-11-16 17:01:38 -0300

    add component header

---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/login/logo-barbershop-header.png'
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+   <Container>
+     <Content>
+       <nav>
+         <img src={logo} alt="barbershop" />
+         <Link to="/dashboard">DASHBOARD</Link>
+       </nav>
+
+       <aside>
+         <Profile>
+           <div>
+             <strong>Alisson Macedo</strong>
+             <Link to="/profile">Meu Perfil</Link>
+           </div>
+           <img src="https://api.adorable.io/avatars/50/abott@adorable.png"
+           alt="Alisson Macedo"
+           />
+         </Profile>
+       </aside>
+     </Content>
+   </Container>
+  );
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #FFF;
+  padding: 0 30px;
+  box-shadow: 0px 0px 5px #000;
+`;
+
+export const Content = styled.div`
+  height: 64px;
+  max-height: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav{
+    display: flex;
+    align-items: center;
+  
+
+  img {
+    height: 50px;
+    width: 65px;
+    margin-right: 20px;
+    padding-right: 20px;
+    border-right: 1px solid #eee;
+  }
+
+  a{
+    font-weight: bold;
+    color: #7159c1;
+  }
+}
+`;
+
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+  border-left: 1px solid #eee;
+
+  div {
+    text-align: right;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #333;
+    }
+  }
+  img {
+      height: 32px;
+      border-radius: 50%;
+    }
+`;
+
